34d8e7d60a376c41f5345912d9efc50f4b272a2b
minddnim <minddnim@gmail.com>
2014-06-08 00:40:34 +0900

    Merge warning log.

--- westonWarning.hs
+++ westonWarning.hs
@@ -1,10 +1,30 @@
 import Control.Monad
-import Control.Applicative()
+import Control.Arrow
 import System.Environment(getArgs)
+import Data.List
+import Data.Ord
+import Text.Printf
 
 main :: IO()
 main = do
   args <- getArgs
-  forM_ args $ \readFilePath -> do
-    contents <- readFile readFilePath
-  writeFile "westonWarning.log" logStr
+  logFiles <- mapM readFile args
+  let contents = map lines logFiles
+  mapM_ (\(t,s) -> printf "%f\t%s\n" t s) $ conv contents 0 []
+
+conv :: [[String]] -> Int -> [[(Double, String)]] -> [(Double, String)]
+conv [] _ ret = sortBy (comparing fst) $ concat ret
+conv (f:fs) n ret = conv fs (n+1) (fileLog:ret)
+  where fLog = convLog 0.0 [] f
+        fileLog = map (second (replicate n '\t' ++)) fLog
+
+convLog :: Double -> [(Double, String)] -> [String] -> [(Double, String)]
+convLog _ ret [] = ret
+convLog t ret (l:ls) = convLog time (r:ret) ls
+  where r@(time, _) = parse t l
+
+parse :: Double -> String -> (Double, String)
+parse time l | length l <= 12 = (time, l)
+             | e == ']' && b == '[' = (read t :: Double, str)
+             | otherwise = (time, l)
+  where (b:t, e:str) = splitAt 12 l
